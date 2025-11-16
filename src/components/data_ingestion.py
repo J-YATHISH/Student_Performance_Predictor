a1811aecd0cc53b3476664b02ee1a2c5d9b08d8e
J-YATHISH <yathishj006@gmail.com>
2025-11-16 18:53:55 +0530

    added

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -4,16 +4,15 @@ from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
 
-from src.components.model_trainer import ModelTrainer
-from src.components.model_trainer import ModelTrainerConfig
-
-from src.components.data_transformation import DataTransformation
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
-@dataclass #Python decorator that makes it easier to create classes that are mainly used to store data.
-
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+@dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts',"train.csv")
     test_data_path: str=os.path.join('artifacts',"test.csv")
@@ -26,15 +25,16 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            df=pd.read_csv('notebook\data\study.csv')
+            df=pd.read_csv('notebook\data\stud.csv')
+            
             logging.info('Read the dataset as dataframe')
 
-            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True) #self.ingestion_config.train_data_path = 'artifacts/train.csv' os.path.dirname('artifacts/train.csv')   # → 'artifacts'
-
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
 
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
 
             logging.info("Train test split initiated")
+            
             train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
 
             train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
@@ -45,6 +45,7 @@ class DataIngestion:
 
             return(
                 self.ingestion_config.train_data_path,
+                
                 self.ingestion_config.test_data_path
 
             )
@@ -53,14 +54,12 @@ class DataIngestion:
         
 if __name__=="__main__":
     obj=DataIngestion()
-    train_data,test_data = obj.initiate_data_ingestion()
-    
-    data_transformation = DataTransformation()
-    
-    train_arr,test_arr,_ = data_transformation.initiate_data_transformation(train_data,test_data)
-    
-    modeltrainer = ModelTrainer()
-    
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
     print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
-    
-    
+
+
